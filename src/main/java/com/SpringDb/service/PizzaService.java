429d80193418d8a6664847f953a513bd3eaef62e
JEdwinFG <edwin_241191@hotmail.com>
2026-04-16 18:29:56 -0600

    Se integra la clase PizzaController y la aplimentacion de JdbcTemplete para consultas SQL en crudo

---
+++ src/main/java/com/SpringDb/service/PizzaService.java
@@ -0,0 +1,26 @@
+package com.SpringDb.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+
+import com.SpringDb.persitence.entity.PizzaEntity;
+
+@Service
+public class PizzaService {
+      
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public PizzaService(JdbcTemplate jdbcTemplate){
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+
+    public List<PizzaEntity> getAll(){
+        return this.jdbcTemplate.query("SELECT* FROM pizza", new BeanPropertyRowMapper(PizzaEntity.class));
+    }
+}
---
+++ src/main/java/com/SpringDb/web/controller/PizzaController.java
@@ -0,0 +1,29 @@
+package com.SpringDb.web.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.SpringDb.persitence.entity.PizzaEntity;
+import com.SpringDb.service.PizzaService;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+@RequestMapping("/api/pizzas")
+public class PizzaController {
+    private final PizzaService pizzaService;
+
+    @Autowired
+    public PizzaController (PizzaService pizzaService){
+        this.pizzaService = pizzaService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<PizzaEntity>> getAll() {
+        return ResponseEntity.ok(this.pizzaService.getAll());
+    }
+}
